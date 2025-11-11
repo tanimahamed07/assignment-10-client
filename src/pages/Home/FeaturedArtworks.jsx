8434d7619b5fc092d68e5468e24b7f170c28b9d8
tanimahmed07 <tanimislamt@gmail.com>
2025-11-11 21:25:01 +0600

    bug fixed

--- src/pages/Home/FeaturedArtworks.jsx
+++ src/pages/Home/FeaturedArtworks.jsx
@@ -13,7 +13,6 @@ const FeaturedArtworks = () => {
         setLoading(true)
         axiosInstance.get('/latest-artworks')
             .then(res => {
-                console.log(res.data.result)
                 setArtworks(res.data.result)
                 setLoading(false)
             })
@@ -29,7 +28,7 @@ const FeaturedArtworks = () => {
                 <h2 className="text-3xl font-bold mb-6">Featured Artworks</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     {artworks.map((art) => (
-                        <ArtsCard art={art}></ArtsCard>
+                        <ArtsCard key={art._id} art={art}></ArtsCard>
                     ))}
                 </div>
             </section>
